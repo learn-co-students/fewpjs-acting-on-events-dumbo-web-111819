491b5d79f9593f138a4520c36f40e475570ea093
Jonathan Brierre <github email address>
2020-01-06 13:41:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+let dodger = document.querySelector('#dodger');
+dodger.style.backgroundColor = 'purple'
+
+document.addEventListener('keydown', e => {
+    if (e.key === 'ArrowLeft'){
+        moveDodgerLeft();
+    }
+    if (e.key === 'ArrowRight'){
+        moveDodgerRight();
+    }
+
+})
+
+let moveDodgerLeft = () =>{
+    let moveLeft = dodger.style.left.replace('px','');
+    let leftMove = parseInt(moveLeft)
+
+    if (leftMove > 0) {
+        dodger.style.left = `${leftMove - 2}px`;
+    }
+}
+
+let moveDodgerRight = () =>{
+        let editLeft = dodger.style.left.replace('px','')
+        let leftValue = parseInt(editLeft)
+        if (leftValue <= 360) {
+            dodger.style.left = `${leftValue + 2}px`;
+        }
+    
+}
